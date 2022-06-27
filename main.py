0365897ecbae9f5eeea4528fb019f65e8ce3f3b3
Huteng <hutengdai@gmail.com>
2022-06-27 13:34:23 -0400

    push

---
+++ main.py
@@ -0,0 +1,20 @@
+
+#TODO
+
+def phonotactics(one, two):
+	# a global WFA or a local PFA: 
+	return x
+
+def process_data():
+	# return 
+	return x
+
+def str2fmat(string, attributes):
+	# comment: return feature matrix (fmat)
+	return fmat
+
+def markov(one, two):
+	# comment: 
+# end def	
+
+
