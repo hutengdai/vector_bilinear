23522c1090843d03dd36417ae9b12541f5057a47
Huteng <hutengdai@gmail.com>
2022-06-28 11:00:59 -0400

    alright

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 
 #TODO
 
-def phonotactics(one, two):
-	# a global WFA or a local PFA: 
+def local_PFA(prefix,logProb):
+	# a local PFA: 
+	# for each features feat(prefix) * w 
+	# for f in 
+
 	return x
 
 def process_data():
@@ -13,8 +16,13 @@ def str2fmat(string, attributes):
 	# comment: return feature matrix (fmat)
 	return fmat
 
-def markov(one, two):
-	# comment: 
-# end def	
-
-
+if __name__ == '__main__':
+	FeatureFile = 'data\\TurkishFeatures-tell.txt'
+	TrainingFile = 'data\\TurkishLearningData-tell.txt'
+	TestingFile = 'data\\TurkishTestingData.txt'
+	sample = get_corpus_data(TrainingFile)
+	alphabet, max_length = process_data(sample)
+	ix2phone = {ix: p for (ix, p) in enumerate(alphabet)}
+	phone2ix = {p: ix for (ix, p) in enumerate(alphabet)}
+	feat, feature_dict, num_feats, feature_table, feat2ix, ix2feat = process_features(FeatureFile, alphabet, ix2phone)
+	vowel = [x for x in feature_dict if feature_dict[x][feat2ix['syll']] == "+" if feature_dict[x][feat2ix['long']] == "-"] #
