9efd635d5b60f8218fd7eaff162054980e8d35cb
Huteng <hutengdai@gmail.com>
2022-09-14 21:16:08 -0400

    meeting update

--- data_viz.py
+++ data_viz.py
@@ -1,12 +1,12 @@
 from platform import machine
 import matplotlib.pyplot as plt
-from scipy.stats import pearsonr, spearmanr
+from scipy.stats import pearsonr, spearmanr, kendalltau
 from statannot import add_stat_annotation
 import numpy as np
 import pandas as pd
 import seaborn as sns
 from scipy.special import softmax
-from plotnine import ggplot, geom_violin, aes, stat_smooth, facet_wrap
+from plotnine import *
 import pprint
 import numpy as np
 
@@ -21,6 +21,7 @@ def visual_judgefile(humanjudgefile,bilinear_judgement):
 		sep=",",
 		header=0,
 		encoding="utf-8")
+
 	machine_data = pd.read_csv(
 		bilinear_judgement,
 		sep=",",
@@ -34,14 +35,23 @@ def visual_judgefile(humanjudgefile,bilinear_judgement):
 	# fig_human.save('daland.png', dpi=300)
 
 	data["machine_judgement"] = machine_data["score"]
-
-	print(data)
+	# newdata = data
+	newdata = data[data["machine_judgement"] > -25]
+	print(newdata)
 	
-	pearsoncorr, _ = pearsonr(data['likert_rating'], data['machine_judgement'])
+	pearsoncorr, _ = pearsonr(newdata['likert_rating'], newdata['machine_judgement'])
 	print('Pearsons correlation: %.3f' % pearsoncorr)
-	spearmancorr, _ = spearmanr(data['likert_rating'], data['machine_judgement'])
+	# 
+	spearmancorr, _ = spearmanr(newdata['likert_rating'], newdata['machine_judgement'])
 	print('Spearman correlation: %.3f' % spearmancorr)
-	fig = sns.scatterplot(data=data, x="machine_judgement", y="likert_rating")
+	kendalltaucorr, _ = kendalltau(newdata['likert_rating'], newdata['machine_judgement'])
+	print('Kentall correlation: %.3f' % kendalltaucorr)
+
+	# fig = ggplot.scatterplot(data=data, x="machine_judgement", y="likert_rating")
+	a = (ggplot(newdata, aes(x='machine_judgement', y='likert_rating', color='attestedness')) + 
+	geom_point())
+
+	print(a)
 	plt.savefig('correlation.png', dpi=300)
 
 	# dir(fig)
@@ -61,3 +71,17 @@ if __name__ == '__main__':
 	bilinear_judgement = "data\\bilinear_judgement.txt"
 	nelson_judgement = "data\\Nelson_model_onset_judgement.txt"
 	visual_judgefile(humanJudgement,nelson_judgement)
+
+# bilinear:
+# Pearsons correlation: -0.798
+# Spearman correlation: -0.842
+# Kentall correlation: -0.651
+# Nelson (<-25):
+# Pearsons correlation: 0.742
+# Spearman correlation: 0.780
+# Kentall correlation: 0.621
+
+# Nelson (>-25):
+# Pearsons correlation: 0.742
+# Spearman correlation: 0.780
+# Kentall correlation: 0.621
