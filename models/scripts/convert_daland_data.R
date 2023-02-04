9e8f9c5878a80d1b50217a7d6727dac5f5988c37
Connor Mayer <cjmayer@uci.edu>
2023-02-03 20:47:16 -0800

    Fixed correlations

--- models/scripts/convert_daland_data.R
+++ models/scripts/convert_daland_data.R
@@ -1,8 +1,24 @@
 library(tidyverse)
-setwd("E:/git_repos/DFM/models")
-daland_data <- read_csv("Daland_etal_2011__AverageScores.csv")
+#setwd("E:/git_repos/DFM/models")
+setwd("C:/Users/conno/git_repos/vector_bilinear/models/")
+daland_data <- read_csv("judgments/Daland_etal_2011__AverageScores.csv")
 
 daland_data_agg <- daland_data %>%
   group_by(onset, attestedness) %>%
   summarize(rating=mean(likert_rating)) %>%
-  write_csv("daland_scores_aggregated.csv")
+  write_csv("judgments/daland_scores_aggregated.csv")
+
+# Make file with duplicates
+results <- read_csv("results/overall_scores.csv") %>%
+  mutate(onset=word) %>%
+  select(-word, -human_response)
+
+full_daland <- daland_data %>%
+  select(onset, likert_rating)
+  
+full_results <- results %>% 
+  inner_join(full_daland, by="onset") %>%
+  relocate(onset, .before = attestedness) %>%
+  relocate(likert_rating, .after=attestedness)
+
+write_csv(full_results, "results/overall_scores_full.csv")
--- models/scripts/get_correlations.R
+++ models/scripts/get_correlations.R
@@ -1,10 +1,11 @@
 library(tidyverse)
 library(corrr)
 library(GGally)
+library(ggrepel)
 
 #setwd("E:/git_repos/DFM/models")
 setwd("C:/Users/conno/git_repos/vector_bilinear/models")
-scores <- read_csv("results/overall_scores.csv")
+scores <- read_csv("results/overall_scores_aggregated.csv")
 
 scores <- scores %>%
   mutate(hayes_phonetic_features = hayes_phonetic_features * -1,
@@ -14,32 +15,85 @@ scores <- scores %>%
          discrete_binary_learned = discrete_binary_learned * -1
   )
 
+scores_full <- read_csv("results/overall_scores.csv")
+
+scores_full <- scores_full %>%
+  mutate(hayes_phonetic_features = hayes_phonetic_features * -1,
+         hayes_learned_features = hayes_learned_features * -1,
+         continuous_pmi = continuous_pmi * -1,
+         discrete_binary_phonetic = discrete_binary_phonetic * -1,
+         discrete_binary_learned = discrete_binary_learned * -1
+  ) 
+
 # Overall correlations
 scores %>% 
-  select(-word) %>%
+  select(-onset) %>%
   group_by() %>%
   nest(data=everything()) %>%
   mutate(
     correlations = map(data, correlate, method='kendall')
   ) %>%
   unnest(correlations) %>% 
-  select(term, human_response) %>%
-  filter(!is.na(human_response)) %>%
-  write_csv('results/overall_correlations.csv')
+  select(term, likert_rating) %>%
+  filter(!is.na(likert_rating)) %>%
+  write_csv('results/overall_correlations_aggregated.csv')
 
 # Grouped correlations
 scores %>% 
-  select(-word) %>%
+  select(-onset) %>%
   group_by(attestedness) %>%
   nest() %>%
   mutate(
     correlations = map(data, correlate, method='kendall')
   ) %>%
   unnest(correlations) %>% 
-  select(attestedness, term, human_response) %>%
-  filter(!is.na(human_response)) %>%
+  select(attestedness, term, likert_rating) %>%
+  filter(!is.na(likert_rating)) %>%
+  write_csv('results/grouped_correlations_aggregated.csv')
+
+# Overall correlations
+scores_full %>% 
+  select(-onset) %>%
+  group_by() %>%
+  nest(data=everything()) %>%
+  mutate(
+    correlations = map(data, correlate, method='kendall')
+  ) %>%
+  unnest(correlations) %>% 
+  select(term, likert_rating) %>%
+  filter(!is.na(likert_rating)) %>%
+  write_csv('results/overall_correlations.csv')
+
+# Grouped correlations
+scores_full %>% 
+  select(-onset) %>%
+  group_by(attestedness) %>%
+  nest() %>%
+  mutate(
+    correlations = map(data, correlate, method='kendall')
+  ) %>%
+  unnest(correlations) %>% 
+  select(attestedness, term, likert_rating) %>%
+  filter(!is.na(likert_rating)) %>%
   write_csv('results/grouped_correlations.csv')
 
-scores %>%
-  select(-word, -attestedness) %>%
-  ggpairs() 
+
+
+plot_scores <- scores %>%
+  pivot_longer(c(-onset, -attestedness, -likert_rating), names_to="model", values_to="score")
+
+plot_scores_full <- scores_full %>%
+  pivot_longer(c(-onset, -attestedness, -likert_rating), names_to="model", values_to="score")
+
+ggplot(plot_scores, aes(likert_rating, score, label=onset)) +
+  geom_point() +
+  geom_text_repel(size=2) +
+  facet_wrap(~model, scale="free")
+ggsave("averaged_models_plot.png")
+
+ggplot(plot_scores_full, aes(likert_rating, score, label=onset)) +
+  geom_point() + 
+  geom_text_repel(size=2) +
+  facet_wrap(~model, scale="free")
+ggsave("models_plot.png")
+
